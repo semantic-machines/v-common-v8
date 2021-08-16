d8abee3fe40a49a1b8b7ffc907e0dff4b3b2b812
Itiua <ValeriyBushenev@gmail.com>
2021-08-16 23:51:52 +0300

    update dependency rusty-v8 to 0.26.0 + icu data

--- src/jsruntime.rs
+++ src/jsruntime.rs
@@ -14,7 +14,7 @@ pub struct JsRuntime {
 }
 
 pub fn v8_init() {
-    let platform = v8::new_default_platform().unwrap();
+    let platform = v8::new_default_platform(0, false).make_shared();
     v8::V8::initialize_platform(platform);
     v8::V8::initialize();
 }
